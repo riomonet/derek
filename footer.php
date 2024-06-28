2f6533bb67e215628fcd21e9ae824e75d85f0bac
Rio Monet <ari@marina59.com>
2024-06-28 17:01:53 -0400

    Added php files

---
+++ footer.php
@@ -0,0 +1,8 @@
+	</main>
+
+	<footer class="site-footer">
+	    <h2>footer...</h2>
+	</footer>
+
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,39 @@
+<!doctype html>
+
+<html lang='en'>
+    <head>
+	<meta charset='utf-8'/>
+	<!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+
+	<title>The webpage title</title>
+	<!-- Meta data  -->
+	<!-- description  -->
+	<!-- image  -->
+
+	<link href="site.css" rel="stylesheet"/>
+    </head>
+    
+    <body>
+      <header class="site-header">
+	    <a href="index.php>
+		<svg viewbox="0 0 10 10" width="40">
+		    <circle cx="5" cy="5" r="5" fill="red" />
+		</svg> 
+	    </a>
+	    <nav class="site-menu">
+		<ul>
+		    <li>
+			<a href="index.php">Home</a>
+		    </li>
+		    <li>
+			<a href="about.php">About</a>
+		    </li>
+		    <li>
+			<a href="contact.php">Contact</a>
+		    </li>
+		</ul>
+	    </nav>
+	</header>
+	
+		
+	<main class="page-content">
