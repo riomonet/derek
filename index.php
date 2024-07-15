be37757fa5862bf8615faf26ef134bc82c8ce824
riomonet <arizablozki@gmail.com>
2024-07-15 12:00:06 -0400

    Added flex2.html

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
 		<li><a href="structure_practice/flex_1.html">Flexbox practicum 1</a></li>
 		<li><a href="structure_practice/The_BC.html">FB2: The BC</a></li>
 		<li><a href="structure_practice/BC-2.html">2nd variation</a></li>
-		<li><a href="structure_practice/BC-3.html">3rd variation</a></li>		
+		<li><a href="structure_practice/BC-3.html">3rd variation</a></li>
+		<li><a href="structure_practice/flex2.html">align-items</a></li>				
 	    </ul>
 	</div>
     </header>
