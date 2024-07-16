4847799ca92cf2efb8703c61faa5269caa7c5b7a
riomonet <arizablozki@gmail.com>
2024-07-15 20:55:05 -0400

    Modified index.php

--- index.php
+++ index.php
@@ -5,12 +5,23 @@
 	<div class="inner-column">
 	    <h1>Practicum</h1>
 	    <ul>
-		<li><a href="structure_practice/flex_1.html">Flexbox practicum 1</a></li>
-		<li><a href="structure_practice/The_BC.html">FB2: The BC</a></li>
-		<li><a href="structure_practice/BC-2.html">2nd variation</a></li>
-		<li><a href="structure_practice/BC-3.html">3rd variation</a></li>
-		<li><a href="structure_practice/BC-4.html">4th variation</a></li>
-		<li><a href="structure_practice/flex2.html" target="_align-items">align-item prep</a></li>
+		<li><a href="structure_practice/flex_1.html"
+		       target="_practicum">Flexbox practicum 1</a></li>
+
+		<li><a href="structure_practice/The_BC.html"
+		       target="_FB2">FB2: The BC</a></li>	
+
+		<li><a href="structure_practice/BC-2.html"
+		       target="_2ndVar">2nd variation</a></li>
+
+		<li><a href="structure_practice/BC-3.html"
+		       target="_3rdvar">3rd variation</a></li>
+
+		<li><a href="structure_practice/BC-4.html"
+		       target="_4thVar">4th variation</a></li>
+
+		<li><a href="structure_practice/flex2.html"
+		       target="_align-items">align-item prep</a></li>
 	    </ul>
 	</div>
     </header>
