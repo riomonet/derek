e6609af35a41835d03b1e5c1379e8d978fa50f11
riomonet <arizablozki@gmail.com>
2024-07-15 20:49:52 -0400

    Added BC-4

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
 		<li><a href="structure_practice/The_BC.html">FB2: The BC</a></li>
 		<li><a href="structure_practice/BC-2.html">2nd variation</a></li>
 		<li><a href="structure_practice/BC-3.html">3rd variation</a></li>
-		<li><a href="structure_practice/flex2.html">align-items</a></li>				
+		<li><a href="structure_practice/BC-4.html">4th variation</a></li>
+		<li><a href="structure_practice/flex2.html" target="_align-items">align-item prep</a></li>
 	    </ul>
 	</div>
     </header>
